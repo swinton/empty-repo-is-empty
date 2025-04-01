cfc00b197a7272c3a7258f76a7d1e5874dae9a45
Steve Winton <stevewinton@gmail.com>
2025-04-01 10:42:51 -0500

    Add Terraform manifest file

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "null_resource" "hello" {
+  provisioner "local-exec" {
+    command = "echo Hello, Terraform!"
+  }
+}
