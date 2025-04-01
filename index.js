fbfdaca75ef6d02937680e0db277cdc45b7e52e0
Steve Winton <stevewinton@gmail.com>
2025-04-01 11:02:19 -0500

    Add vulnerable first-party code

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const _ = require("lodash");
 
-process.stdout.write("Hello world\n");
+const expression = process.argv.slice(2).join(' ').concat(';');
+try {
+  console.log(eval(expression));
+} catch {
+  console.log(`Error: Unable to evaluation ${expression}. Maybe try that again?`);
+  process.exit(1);
+}
